efd6ee270ef1dc196db4e295e4e7e4a9037acf71
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-11-07 10:02:00 -0300

    Atualização 07/10/2022 10:10

--- BaseCapturaBluetooth.ino
+++ BaseCapturaBluetooth.ino
@@ -18,25 +18,26 @@ int Eixo_Y=0;
 
 //Pinagem para o braço robotico.
 //motor 1
-#define SERVO1 A0 // Variável Servo
+#define SERVO1 A1 // Variável Servo - Servo para levantar e baixar o braço
 int SERVO1_posicao = 60; // Posição Servo
 
 //motor 2
-#define SERVO2 A1 // Variável Servo
+#define SERVO2 A2 // Variável Servo - Servo do avanço do braço, para frente e para trás.
 int SERVO2_posicao = 130; // Posição Servo - Início retraido em 130, vai de 0 a 180;
 
 //motor 3
-#define SERVO3 A2 // Variável Servo
-int SERVO3_posicao = 140; // Posição Servo - //Início fechado em 140, vai de 160 a 60;
+#define SERVO3 A3 // Variável Servo - Servo da gara
+int SERVO3_posicao = 140; // Posição Servo - //Início fechado em 140, vai de 75 a 150;
 
 //motor 4
-#define SERVO4 A3 // Variável Servo
-int SERVO4_posicao = 80; // Posição Servo
+#define SERVO4 A4 // Variável Servo - Servo da base giratória
+int SERVO4_posicao = 80; // Posição Servo //Início centralizado em 80, vai de 0 a 160;
 
 
 void setup() {
   pinMode(SERVO1, OUTPUT);
-  moverServo(SERVO1,SERVO1_posicao);
+  //obs: "moverServo(Servo,posição)" é uma função que criei para não depender de bibliotecas para este fim.
+  moverServo(SERVO1,SERVO1_posicao); 
 
   pinMode(SERVO2, OUTPUT);
   moverServo(SERVO2,SERVO2_posicao);
@@ -47,7 +48,7 @@ void setup() {
   pinMode(SERVO4, OUTPUT);
   moverServo(SERVO4,SERVO4_posicao); 
 
-  delay(1000);
+  delay(1000); // Colocar os Servos na posição ao inicializar o Arduino
 
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
@@ -56,27 +57,23 @@ void setup() {
   pinMode(POTENCIA_E,OUTPUT);
   pinMode(POTENCIA_D,OUTPUT);
   Serial.begin(9600);
-  BT.begin(9600); // Default communication rate of the Bluetooth module
+  BT.begin(9600); 
   delay(500);
 }
 
 void loop() { 
 
-  while (BT.available() >= 2) {
+  if (BT.available() > 1) { // Se o bluetooth enviar pelomentos dois dados (X, Y) inicializamos a captura dos dados
     Eixo_X = BT.read();
-    delay(10);
     Eixo_Y = BT.read();
 
-    if(Eixo_X < 201 && Eixo_Y < 201){      
-        Eixo_X -= 100;        
+    if(Eixo_X < 201 && Eixo_Y < 201){ //Se os dados enviados forem do primeira joystick (Direção do tamque) eles teram valor entre 0 e 200
+        Eixo_X -= 100;          //convertendo os dados para um plano cartesiano de origem x=0 e y=0 e range que vai de -100 até 100 para x e y;    
         Eixo_Y = 100 - Eixo_Y;   
     }
-    printDados(Eixo_X, Eixo_Y);
-    acao(Eixo_X, Eixo_Y);
-  }
-  acao(Eixo_X, Eixo_Y);
-  delay(10);
-  
+  }  
+  acao(Eixo_X, Eixo_Y); //A função ação verifica o que deve ser feito ao receber os dados;
+    
 }
 
 void moverServo(uint8_t servo, int angulo)              
@@ -90,26 +87,67 @@ void moverServo(uint8_t servo, int angulo)
 }
 
 void acao(int x, int y){
-  if(x > -101 && x < 101){
+  if(x > -101 && x < 101){    
     direcao(x, y);
   }
-  else if(x==210){
-    if(SERVO3_posicao > 75){
-      SERVO3_posicao--;    
-      // moverServo(SERVO3, SERVO3_posicao);
-    }    
-  }
-  else if(x==211){
-    if(SERVO3_posicao < 148){
-      SERVO3_posicao++;    
-      // moverServo(SERVO3, SERVO3_posicao);
+  if(x==213){ //Levantar braço
+    if(SERVO1_posicao < 170){
+      printDados(Eixo_X, Eixo_Y);
+      SERVO1_posicao++; 
     }  
   }
-  moverServo(SERVO3, SERVO3_posicao);
+  if(x==212){ //Baixar braço
+    if(SERVO1_posicao > 10){
+      printDados(Eixo_X, Eixo_Y);
+      SERVO1_posicao--;   
+    }  
+  }
+  if(x==215){ //Avançar braço para frente
+    if(SERVO2_posicao < 180){
+      printDados(Eixo_X, Eixo_Y);
+      SERVO2_posicao++;  
+    }  
+  }
+  if(x==216){ //Retrair braço para trás
+    if(SERVO2_posicao > 0){
+      printDados(Eixo_X, Eixo_Y);
+      SERVO2_posicao--;    
+    }    
+  }
+  if(x==210){ //Abrir gara
+    if(SERVO3_posicao > 75){
+      printDados(Eixo_X, Eixo_Y);
+      SERVO3_posicao--;    
+    }    
+  }
+  if(x==211){ //Fechar gara
+    if(SERVO3_posicao < 150){
+      printDados(Eixo_X, Eixo_Y);
+      SERVO3_posicao++;  
+    }  
+  }
+  if(x==217){ //Girar base para esquerda
+    if(SERVO4_posicao > 0){
+      printDados(Eixo_X, Eixo_Y);
+      SERVO4_posicao--;    
+    }    
+  }
+  if(x==218){ //Girar base para direita
+    if(SERVO4_posicao < 161){
+      printDados(Eixo_X, Eixo_Y);
+      SERVO4_posicao++;  
+    }  
+  }
+
+  moverServo(SERVO1, SERVO1_posicao);
+  moverServo(SERVO2, SERVO2_posicao);
+  moverServo(SERVO3, SERVO3_posicao); 
+  moverServo(SERVO4, SERVO4_posicao); 
 }
 
 void direcao(int x, int y){    
-    if(x != 0 | y != 0){// Tanque em movimento        
+    if(x != 0 | y != 0){// Tanque em movimento
+        printDados(Eixo_X, Eixo_Y);   
         if(x > -11 && x < 11){// Andar em linha reta          
             if(y > 0){// Andar para frente            
               int potencia = map(y,0,100,0,255);
@@ -172,9 +210,9 @@ void girarParaEsquerda(int potencia){
 }
 void printDados(int x, int y){
   Serial.print("X:"); 
-  Serial.print(Eixo_X); 
+  Serial.print(x); 
   Serial.print(", Y:"); 
-  Serial.println(Eixo_Y);
+  Serial.println(y);
 }
 void motorEsquerdo(int sentido, int potencia){
   analogWrite(POTENCIA_E,potencia);
